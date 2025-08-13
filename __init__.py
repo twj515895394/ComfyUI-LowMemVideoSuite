de850ad189bbba714cdabdb881fef599b33ae05d
ryan.tang <ryan.tang@bufflogistics.com>
2025-08-13 20:37:07 +0800

    refactor(nodes): 重构节点代码并优化功能
    - 重构 SaveSingleFrameToDisk 和 SaveFrameBatchToDisk 节点
    - 新增 tensor_to_preview 函数用于生成节点预览
    - 优化 FFmpegVideoCombineLowMem节点的实现
    - 重构 utils 文件，新增 parse_time_tokens 和 save_comfyui_image 函数

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import *
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
 __version__ = "0.2.0"
