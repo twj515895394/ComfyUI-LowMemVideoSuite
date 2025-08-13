e6ed6090d4eac602ce9a0f7c5eefcfcaf1e4b422
ryan <twj515895394@users.noreply.github.com>
2025-08-13 12:07:26 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+# __init__.py
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+__version__ = "0.2.0"
